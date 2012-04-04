fb00c78003ddc230229cb09e9aa27237e0c53a58
Valentin Haenel <valentin.haenel@gmx.de>
2012-04-04 16:01:08 +0200

    pylint cleanups

--- example.py
+++ example.py
@@ -11,17 +11,17 @@ from multiprocessing import Pool
 from progresspool import ProgressPool
 
 def f(x):
-    time.sleep(random.uniform(0,3))
+    time.sleep(random.uniform(0, 3))
     return x*x
 
 if __name__ == '__main__':
     print "Using a normal pool... you never know when it is done..."
-    p = Pool()
-    p.map(f, range(100))
+    pool = Pool()
+    pool.map(f, range(100))
     print "Oh... finally... it has completed...\n"
 
     time.sleep(3)
 
     print "Now using a progress pool... enjoy the ride! :D"
-    pp = ProgressPool()
-    pp.map(f, range(100))
+    ppool = ProgressPool()
+    ppool.map(f, range(100))
