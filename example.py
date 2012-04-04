42abf697f17f91bff388d65459117be5f0f29e25
Valentin Haenel <valentin.haenel@gmx.de>
2012-04-04 15:19:40 +0200

    add a proper example

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" An example of using the `ProgressPool`
+
+@author  Valentin Haenel <valentin.haenel@epfl.ch>
+
+"""
+
+import time
+from progresspool import ProgressPool
+
+def f(x):
+    time.sleep(random.uniform(0,3))
+    return x*x
+
+if __name__ == '__main__':
+    pp = ProgressPool()
+    print pp.map(f, range(100))
